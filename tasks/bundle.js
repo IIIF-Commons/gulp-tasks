137547b662d17293ab107e90dc835b8073bbee52
edsilv <e.silverton@gmail.com>
2016-11-23 19:00:47 +0000

    use dtsOut

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     gulp.task('bundle:typings', function(cb) {
         return gulp.src(config.dependencies.typings.concat([
-                path.join(config.directories.typings, config.fileNames.dtsBundleOut), // include optional typings/name.d.ts for customisations
+                path.join(config.directories.typings, config.fileNames.dtsOut), // include optional typings/name.d.ts for customisations
                 path.join(config.directories.dist, config.fileNames.dtsOut)
             ]))
             .pipe(concat(config.fileNames.dtsBundleOut))
