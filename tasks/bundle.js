0687e4291796f5ad72212106b69f786c6a106660
edsilv <e.silverton@gmail.com>
2016-11-23 23:18:28 +0000

    combine typings

--- tasks/bundle.js
+++ tasks/bundle.js
@@ -14,12 +14,24 @@ module.exports = function(config) {
     });
 
     gulp.task('bundle:typings', function(cb) {
-        return gulp.src(config.dependencies.typings.concat([
-                path.join(config.directories.typings, config.fileNames.dtsOut), // include optional typings/name.d.ts for customisations
+        return merge([
+            
+            // merge typings/name.d.ts with dist/name.d.ts
+            gulp.src([
+                path.join(config.directories.typings, config.fileNames.dtsOut),
+                path.join(config.directories.dist, config.fileNames.dtsOut)
+            ])
+            .pipe(concat(config.fileNames.dtsOut))
+            .pipe(gulp.dest(config.directories.dist)),
+
+            // merge typings dependencies array and dist/name.d.ts to dist/name.bundle.d.ts
+            gulp.src(config.dependencies.typings.concat([
                 path.join(config.directories.dist, config.fileNames.dtsOut)
             ]))
             .pipe(concat(config.fileNames.dtsBundleOut))
-            .pipe(gulp.dest(config.directories.dist));
+            .pipe(gulp.dest(config.directories.dist))
+
+        ]);
     });
 
 }
